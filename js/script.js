8c4498d1062f61439b6eb896c520adeb9af933b8
Carlos J Pinheiro <carlos.pnx@gmail.com>
2022-10-14 20:23:38 -0300

    adiciona funcionalidade de login

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
-async function injectContent() {
-    let url = location.href;
-    file = url.split('=');
-    let view = (file[1]) ? file[1] : 'inicio';
-    const resp = await fetch(`views/${view}.html`);
-    const html = await resp.text();
-    let inject = document.getElementById('content');
-    inject.innerHTML = html;
-}
+//Função de injeção de paginas na index
 
-injectContent();
+// async function injectContent() {
+//     let url = location.href;
+//     file = url.split('=');
+//     let view = (file[1]) ? file[1] : 'inicio';
+//     const resp = await fetch(`views/${view}.html`);
+//     const html = await resp.text();
+//     let inject = document.getElementById('content');
+//     inject.innerHTML = html;
+// }
+
+// injectContent();
