beffb5a88a68d8ab08aa5fc88b3d01015e080324
Osma301 <Osma301@gmail.com>
2022-10-16 13:29:38 -0300

    suport page structured

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 async function injectContent() {
-    let url = location.href;
-    file = url.split('=');
-    let view = (file[1]) ? file[1] : 'inicio';
-    const resp = await fetch(`views/${view}.html`);
-    const html = await resp.text();
-    let inject = document.getElementById('content');
-    inject.innerHTML = html;
+  let url = location.href;
+  file = url.split('=');
+  let view = file[1] ? file[1] : 'inicio';
+  const resp = await fetch(`views/${view}.html`);
+  const html = await resp.text();
+  let inject = document.getElementById('content');
+  inject.innerHTML = html;
 }
-
 injectContent();
+// Scroll reveal
